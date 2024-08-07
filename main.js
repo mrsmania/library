ac8fc3d0708197f5d4e49314a5e8feb172819a65
mrsmania <122042211+mrsmania@users.noreply.github.com>
2024-08-07 23:44:47 +0200

    changed buttons to icons & started implementing the read flag

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const addBookBtn = document.getElementById('addBookBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const dialog = document.querySelector("dialog");
 const form = document.querySelector("form");
-
+const BOOK_ICON_FINISHED = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon"><path d="M19 2L14 6.5V17.5L19 13V2M6.5 5C4.55 5 2.45 5.4 1 6.5V21.16C1 21.41 1.25 21.66 1.5 21.66C1.6 21.66 1.65 21.59 1.75 21.59C3.1 20.94 5.05 20.5 6.5 20.5C8.45 20.5 10.55 20.9 12 22C13.35 21.15 15.8 20.5 17.5 20.5C19.15 20.5 20.85 20.81 22.25 21.56C22.35 21.61 22.4 21.59 22.5 21.59C22.75 21.59 23 21.34 23 21.09V6.5C22.4 6.05 21.75 5.75 21 5.5V19C19.9 18.65 18.7 18.5 17.5 18.5C15.8 18.5 13.35 19.15 12 20V6.5C10.55 5.4 8.45 5 6.5 5Z" /></svg>';
+const BOOK_ICON_READING = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103.19 122.88" class="icon"><path d="M17.16 0h82.72a3.32 3.32 0 013.31 3.31v92.32c-.15 2.58-3.48 2.64-7.08 2.48H15.94c-4.98 0-9.05 4.07-9.05 9.05s4.07 9.05 9.05 9.05h80.17v-9.63h7.08v12.24c0 2.23-1.82 4.05-4.05 4.05H16.29C7.33 122.88 0 115.55 0 106.59V17.16C0 7.72 7.72 0 17.16 0zm3.19 13.4h2.86c1.46 0 2.66.97 2.66 2.15v67.47c0 1.18-1.2 2.15-2.66 2.15h-2.86c-1.46 0-2.66-.97-2.66-2.15V15.55c.01-1.19 1.2-2.15 2.66-2.15z" fill-rule="evenodd" clip-rule="evenodd"/></svg>';
+const DELETE_ICON = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
 
 addBookBtn.addEventListener('click', () => {
     dialog.showModal();
@@ -36,7 +38,7 @@ function Book(title, author, pages, read) {
 
 Book.prototype.changeReadStatus = function (element) {
     this.read = !this.read;
-    element.textContent = "Read: " + this.read;
+    element.innerHTML = this.read ? BOOK_ICON_READING : BOOK_ICON_FINISHED;
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
@@ -61,21 +63,24 @@ function displayBook(book, index) {
     bookCardInfos.innerHTML = "<p><h2>" + book.title + "</h2></p>";
     bookCardInfos.innerHTML += "<p>" + book.author + "</p>";
     bookCardInfos.innerHTML += "<p>" + book.pages + " pages</p>";
-    bookCardInfos.innerHTML += "<p class=\"status\">Read: " + book.read + "</p>";
-    
-    const statusElement = bookCard.querySelector(".status");
-    const toggleReadBtn = document.createElement('button');
-    toggleReadBtn.textContent ="Toggle read";
-    toggleReadBtn.addEventListener('click', () => book.changeReadStatus(statusElement));
-    bookCardButtons.append(toggleReadBtn);
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener('click', () => {
+    const readIcon = document.createElement('div');
+    readIcon.classList.add("readIcon");
+    readIcon.innerHTML = book.read ? (BOOK_ICON_READING + "<div class=\"read\"></div>") : BOOK_ICON_FINISHED;
+    bookCardButtons.append(readIcon);
+    const readStatusElement = bookCardButtons.querySelector(".readIcon");
+    readIcon.addEventListener('click', () => book.changeReadStatus(readStatusElement));
+
+    const deleteIcon = document.createElement('div');
+    deleteIcon.classList.add("deleteIcon");
+    deleteIcon.innerHTML = DELETE_ICON;
+    bookCardButtons.append(deleteIcon);
+
+    const deleteElement = bookCardButtons.querySelector(".deleteIcon");
+    deleteIcon.addEventListener('click', () => {
         myLibrary.splice(index, 1);
         displayBooks(myLibrary);
     });
-    bookCardButtons.append(deleteBtn);
 }
 
 function displayBooks(array) {
